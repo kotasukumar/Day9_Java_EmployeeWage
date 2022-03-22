48f6b4792c001fba74da6c4809ab673501c146ec
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-22 22:30:51 +0530

    added: attendence logic

--- Main.java
+++ Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        double random = Math.floor(Math.random() * 10) % 2;
+        if (random == 1) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
+
