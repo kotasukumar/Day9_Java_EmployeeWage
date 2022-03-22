e67b6da220a893981ce4749a3227bf872e76e6fc
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-22 22:43:03 +0530

    added:part time employee logic

--- Main.java
+++ Main.java
@@ -3,13 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int ISFULLTIME = 1;
+        int ISPARTTIME = 2;
         int PerHourWage = 20;
         int WorkingHours = 0;
 
-        double random = Math.floor(Math.random() * 10) % 2;
-        if(random == 1){
+        double random = Math.floor(Math.random() * 10) % 3;
+        if(random == ISFULLTIME){
             WorkingHours = 8;
-            System.out.println("Employee is present");
+            System.out.println("Full time employee is present");
+        }
+        else if (random == ISPARTTIME){
+            WorkingHours = 8;
+            System.out.println("Part time employee present");
         }
         else{
             WorkingHours = 0;
