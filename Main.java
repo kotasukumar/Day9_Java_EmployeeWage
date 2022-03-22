c7b4a49b7a7906a0c2cca65da1987fddb55fc5a9
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-22 22:59:00 +0530

    added:for loop

--- Main.java
+++ Main.java
@@ -6,23 +6,30 @@ public class Main {
         final int ISFULLTIME = 1;
         final int ISPARTTIME = 2;
         int PerHourWage = 20;
-        int WorkingHours = 0;
+        int workingHours = 0;
+        int monthlyHours = 0;
+        int totalWorkingDays = 1;
 
-        int random = (int) Math.floor(Math.random() * 10) % 3;
-        switch (random) {
-            case ISFULLTIME :
-                WorkingHours = 8;
-                System.out.println("Full time employee is present");
-                break;
-            case ISPARTTIME:
-                WorkingHours = 8;
-                System.out.println("Part time employee present");
-                break;
-            default:
-                WorkingHours = 0;
-                System.out.println("Employee is absent");
+        for (int i = 0; i < 21; i++) {
+            int random = (int) Math.floor(Math.random() * 10) % 3;
+            switch (random) {
+                case ISFULLTIME:
+                    workingHours = 8;
+                    System.out.println("Full time employee is present");
+                    totalWorkingDays++;
+                    break;
+                case ISPARTTIME:
+                    workingHours = 8;
+                    System.out.println("Part time employee present");
+                    totalWorkingDays++;
+                    break;
+                default:
+                    workingHours = 0;
+                    System.out.println("Employee is absent");
+            }
+            monthlyHours = monthlyHours + workingHours;
         }
-        int daily_wage = PerHourWage * WorkingHours;
+        int daily_wage = PerHourWage * monthlyHours;
         System.out.println("today wage is: " + daily_wage);
     }
 }
