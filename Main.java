04d09c3ba3090223629020590d7fa2390e08b19e
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-22 22:50:55 +0530

    added:switch case

--- Main.java
+++ Main.java
@@ -3,23 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int ISFULLTIME = 1;
-        int ISPARTTIME = 2;
+        final int ISFULLTIME = 1;
+        final int ISPARTTIME = 2;
         int PerHourWage = 20;
         int WorkingHours = 0;
 
-        double random = Math.floor(Math.random() * 10) % 3;
-        if(random == ISFULLTIME){
-            WorkingHours = 8;
-            System.out.println("Full time employee is present");
-        }
-        else if (random == ISPARTTIME){
-            WorkingHours = 8;
-            System.out.println("Part time employee present");
-        }
-        else{
-            WorkingHours = 0;
-            System.out.println("Employee is absent");
+        int random = (int) Math.floor(Math.random() * 10) % 3;
+        switch (random) {
+            case ISFULLTIME :
+                WorkingHours = 8;
+                System.out.println("Full time employee is present");
+                break;
+            case ISPARTTIME:
+                WorkingHours = 8;
+                System.out.println("Part time employee present");
+                break;
+            default:
+                WorkingHours = 0;
+                System.out.println("Employee is absent");
         }
         int daily_wage = PerHourWage * WorkingHours;
         System.out.println("today wage is: " + daily_wage);
